8961be8b2a63b5d9ff665209f3c8c36bbc5a19d7
kevinfiol <me@kevinfiol.com>
2020-08-30 01:46:13 -0400

    remove cors

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ const corsOpts = {
 
 // middleware
 app.use(helmet());
-app.use(cors(corsOpts));
+app.use(cors());
 app.use(parse.json());
 app.use((_, res, next) => {
     res.send = send.bind(null, res);
