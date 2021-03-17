43454dc006aa72aff88dd086a9f35db28c59cd1a
Kevin Fiol <me@kevinfiol.com>
2021-03-16 22:51:28 -0400

    Update UserRoutes.js

--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -1,25 +1,22 @@
 const polka = require('polka');
+const cors = require('cors');
 const UserController = require('../controllers/UserController.js');
 
 const router = polka();
 
-const cors = (_, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*"); 
-    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-};
+const routeCors = cors({ origin: '*' });
 
-router.options('/getLibraryResult', cors);
-router.options('/getCommonApps', cors);
-router.options('/updateLibraryResult', cors);
+router.options('/getLibraryResult', routeCors);
+router.options('/getCommonApps', routeCors);
+router.options('/updateLibraryResult', routeCors);
 
 // GET
 router.get('/getAllSteamCategories', UserController.getAllSteamCategories);
 router.get('/getAllProfiles/:identifier', UserController.getAllProfiles);
 
 // POST
-router.post('/getLibraryResult', UserController.getLibraryResult);
-router.post('/getCommonApps', UserController.getCommonApps);
-router.post('/updateLibraryResult', UserController.updateLibraryResult);
+router.post('/getLibraryResult', routeCors, UserController.getLibraryResult);
+router.post('/getCommonApps', routeCors, UserController.getCommonApps);
+router.post('/updateLibraryResult', routeCors, UserController.updateLibraryResult);
 
 module.exports = router;
