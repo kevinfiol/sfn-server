18eaad60c5f381c2dea675ddd9b374f842fb77b4
Kevin Fiol <me@kevinfiol.com>
2021-03-16 22:39:30 -0400

    Update UserRoutes.js

--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -3,6 +3,12 @@ const UserController = require('../controllers/UserController.js');
 
 const router = polka();
 
+router.use(function(_, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "*"); 
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 // GET
 router.get('/getAllSteamCategories', UserController.getAllSteamCategories);
 router.get('/getAllProfiles/:identifier', UserController.getAllProfiles);
