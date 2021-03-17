78b42f525f9c9182930634254ec24750a2c5fa87
kevinfiol <me@kevinfiol.com>
2021-03-17 01:05:51 -0400

    cors on user routes

--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -3,6 +3,13 @@ const UserController = require('../controllers/UserController.js');
 
 const router = polka();
 
+router.use((_, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*"); 
+    res.setHeader("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+    next();
+});
+
 router.options('*', (_, res, next) => {
     res.end('ok');
 });
