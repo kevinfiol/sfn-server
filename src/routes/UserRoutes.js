f7fcc6a312eecde9316ec36d45d1a544068e0e9b
kevinfiol <me@kevinfiol.com>
2021-03-21 12:28:46 -0400

    cors

--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -1,7 +1,16 @@
 const polka = require('polka');
+const cors = require('cors');
 const UserController = require('../controllers/UserController.js');
 
+// opts
+const corsOpts = {
+    origin: '*',
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    optionsSuccessStatus: 200
+};
+
 const router = polka();
+router.use(cors(corsOpts));
 
 // GET
 router.get('/getAllSteamCategories', UserController.getAllSteamCategories);
